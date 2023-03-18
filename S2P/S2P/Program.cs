6a4a6bdafe1d103228b8ab6864ce4ade252648fe
Angeld10 <122924730+Angeld10@users.noreply.github.com>
2023-03-17 21:16:10 -0700

    interpolation done

--- S2P/S2P/Program.cs
+++ S2P/S2P/Program.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using Microsoft.VisualBasic;
-using Microsoft.VisualBasic.FileIO;
+﻿using Microsoft.VisualBasic.FileIO;
 
 namespace S2P
 {
@@ -8,31 +6,121 @@ namespace S2P
     {
         static void Main()
         {
-            var path = Console.ReadLine();
-            if (path == null) throw new Exception("null file");
-            TextFieldParser parser = new(path);
-            parser.TextFieldType = FieldType.Delimited;
-            parser.SetDelimiters("  ");
+            var s2plog = new S2PLog
+            {
+                TouchstoneVersion2 = false
+            };
+
+            var timestamp = DateTime.Now;
+
+            Console.Write("S2P File Path: ");
+            var path = Console.ReadLine() ?? throw new Exception("null file");
+            TextFieldParser parser = new(path)
+            {
+                TextFieldType = FieldType.Delimited
+            };
+            parser.SetDelimiters(" ");
             string?[] header = new string[11];
+            List<double> magS11 = new();
+            List<double> magS21 = new();
+            List<double> magS12 = new();
+            List<double> magS22 = new();
+            List<double> angS11 = new();
+            List<double> angS21 = new();
+            List<double> angS12 = new();
+            List<double> angS22 = new();
+            List<double> freqs = new();
+            List<double> SparamValues = new();
+
             for (int i = 0; i < 11; i++)
             {
                 //Processing row
                 header[i] = parser.ReadLine();
             }
-            for (int i = 0; i < 50; i++)
+            while (!parser.EndOfData)
             {
                 var fields = parser.ReadFields();
                 if(fields != null)
                 {
                     foreach (string field in fields)
                     {
-                        Console.WriteLine(i);
-                        Console.WriteLine(field);
+                        if (!field.Contains(' ') && field != "")
+                        {
+                            SparamValues.Add(Convert.ToDouble(field));
+                        }
                     }
                 }
-                
             }
-                
+            for (int i = 0; i < SparamValues.Count; i += 9)
+            {
+
+                freqs.Add(SparamValues[i]);
+                magS11.Add(SparamValues[i + 1]);
+                angS11.Add(SparamValues[i + 2]);
+
+                magS21.Add(SparamValues[i + 3]);
+                angS21.Add(SparamValues[i + 4]);
+
+                magS12.Add(SparamValues[i + 5]);
+                angS12.Add(SparamValues[i + 6]);
+
+                magS22.Add(SparamValues[i + 7]);
+                angS22.Add(SparamValues[i + 8]);
+            }
+            Console.Write("Interpolation Factor: ");
+            int interpolFactor = Convert.ToInt32(Console.ReadLine());
+            List<double> Interpolatedfreqs = InterpolateData(freqs, interpolFactor);
+            List<double> InterpolatedmagS11 = InterpolateData(magS11, interpolFactor);
+            List<double> InterpolatedangS11 = InterpolateData(angS11, interpolFactor);
+            List<double> InterpolatedmagS21 = InterpolateData(magS21, interpolFactor);
+            List<double> InterpolatedangS21 = InterpolateData(angS21, interpolFactor);
+            List<double> InterpolatedmagS12 = InterpolateData(magS12, interpolFactor);
+            List<double> InterpolatedangS12 = InterpolateData(angS12, interpolFactor);
+            List<double> InterpolatedmagS22 = InterpolateData(magS22, interpolFactor);
+            List<double> InterpolatedangS22 = InterpolateData(angS22, interpolFactor);
+
+            s2plog.SetFrequencies(Interpolatedfreqs);
+            s2plog.SetColumnData("magS11", InterpolatedmagS11);
+            s2plog.SetColumnData("angS11", InterpolatedangS11);
+
+            s2plog.SetColumnData("magS21", InterpolatedmagS21);
+            s2plog.SetColumnData("angS21", InterpolatedangS21);
+
+            s2plog.SetColumnData("magS12", InterpolatedmagS12);
+            s2plog.SetColumnData("angS12", InterpolatedangS12);
+
+            s2plog.SetColumnData("magS22", InterpolatedmagS22);
+            s2plog.SetColumnData("angS22", InterpolatedangS22);
+
+            s2plog.AddComments(new Dictionary<string, string> { { "Interpolation Time Stamp", $"{timestamp:MM/dd/yyyy hh/mm/ss tt}" } });
+            s2plog.AddComments(new Dictionary<string, string> { {"Custom Data Interpolation Factor", $"{interpolFactor}"} });
+
+            string fileName = $"INTERPOLATED_{path[(path.LastIndexOf('\\') + 1)..]}";
+            string s2pFilePath = $"{path[..(path.LastIndexOf('\\') + 1)]}{fileName}";
+            s2plog.Save(s2pFilePath);
+        }
+        public static List<double> InterpolateData(List<double> inputData, int interpolationFactor)
+        {
+            List<double> outputData = new();
+
+            for (int i = 0; i < inputData.Count - 1; i++)
+            {
+                double start = inputData[i];
+                double end = inputData[i + 1];
+
+                outputData.Add(start);
+
+                double interval = (end - start) / (interpolationFactor + 1);
+                for (int j = 1; j <= interpolationFactor; j++)
+                {
+                    double value = start + (j * interval);
+                    outputData.Add(value);
+                }
+            }
+
+            outputData.Add(inputData[^1]);
+
+            return outputData;
         }
     }
 }
---
+++ S2P/S2P/S2PLog.cs
@@ -0,0 +1,142 @@
+﻿using System.Data;
+
+namespace S2P
+{
+    public class S2PLog
+    {
+        private readonly DataTable Table;
+        private readonly List<Dictionary<string, string>> Comments;
+        public readonly string[] ColumnNames = new string[] { "freq", "magS11", "angS11", "magS21", "angS21", "magS12", "angS12", "magS22", "angS22" };
+        private const int S2PPadding = 16;
+        public bool TouchstoneVersion2 { get; set; } = true;
+        public S2PLog()
+        {
+            Table = new DataTable();
+
+            foreach (string columnName in ColumnNames)
+            {
+                DataColumn column;
+
+                column = new DataColumn
+                {
+                    ColumnName = columnName,
+                    DataType = typeof(double),
+                    AllowDBNull = false,
+                    DefaultValue = 0
+                };
+                Table.Columns.Add(column);
+            }
+
+            Comments = new List<Dictionary<string, string>> { };
+        }
+        public void ClearDataRows()
+        {
+            Table.Rows.Clear();
+        }
+        public void SetFrequencies(List<double> frequencies)
+        {
+            ClearDataRows();
+            foreach (double frequency in frequencies)
+            {
+                DataRow row;
+                row = Table.NewRow();
+                row["freq"] = frequency;
+                Table.Rows.Add(row);
+            }
+        }
+
+        public void SetColumnData(string columnName, List<double> data)
+        {
+            if (data.Count != Table.Rows.Count)
+                throw new Exception("Data length must match frequency length");
+
+            if (!Table.Columns.Contains(columnName))
+                throw new Exception(columnName + " is not a valid column name");
+
+            for (int i = 0; i < data.Count; i++)
+            {
+                Table.Rows[i][columnName] = data[i];
+            }
+        }
+        public void ClearComments()
+        {
+            Comments.Clear();
+        }
+        //public void AddComments(ILogStateInformation source)
+        //{
+        //    Comments.Add(source.StateInformation);
+        //}
+        public void AddComments(Dictionary<string, string> comments)
+        {
+            Comments.Add(comments);
+        }
+        public void Save(string filename)
+        {
+            int dataLength = Table.Rows.Count;
+
+            // Delete the file if it exists.
+            if (File.Exists(filename))
+            {
+                File.Delete(filename);
+            }
+
+            // Create the file.
+            using StreamWriter fs = new(filename);
+            // Write all comments
+            foreach (var collection in Comments)
+            {
+                foreach (var comment in collection)
+                {
+                    fs.WriteLine("! " + comment.Key + " = " + comment.Value);
+                }
+            }
+
+            if (TouchstoneVersion2)
+            {
+                fs.WriteLine("[Version] 2.0"); //Touchstone version 2
+            }
+            fs.WriteLine("# MHz S DB R 50"); // S params using DB for magnitude and Degrees for phase
+            if (TouchstoneVersion2)
+            {
+                fs.WriteLine("[Number of Ports] 2");
+                fs.WriteLine("[Two-Port Data Order] 21_12");
+                fs.WriteLine("[Number of Frequencies] " + dataLength);
+            }
+
+            // Write column names
+            bool first = true;
+            foreach (DataColumn column in Table.Columns)
+            {
+                string tempString;
+                if (first)
+                {
+                    tempString = "! " + column.ColumnName;
+                    first = false;
+                }
+                else
+                {
+                    tempString = column.ColumnName;
+                }
+
+                fs.Write(tempString.PadRight(S2PPadding));
+            }
+            fs.WriteLine();
+
+            // Write data
+            foreach (DataRow row in Table.Rows)
+            {
+                foreach (DataColumn column in Table.Columns)
+                {
+                    object value = row[column.ColumnName];
+                    if (value == DBNull.Value)
+                        fs.Write("0 ".PadRight(S2PPadding));
+                    else
+                        fs.Write(((double)value).ToString("E7").PadRight(S2PPadding));
+                }
+                fs.WriteLine();
+            }
+
+            fs.Close();
+        }
+    }
+}
