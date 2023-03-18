a30c9d25f52aec2af5b52ade6eb53f2c9ed1f703
LabPC001 <sknapp@eridan.io>
2023-03-17 17:26:05 -0700

    start

---
+++ S2P/S2P/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualBasic;
+using Microsoft.VisualBasic.FileIO;
+
+namespace S2P
+{
+    public class S2PApp
+    {
+        static void Main()
+        {
+            var path = Console.ReadLine();
+            if (path == null) throw new Exception("null file");
+            TextFieldParser parser = new(path);
+            parser.TextFieldType = FieldType.Delimited;
+            parser.SetDelimiters("  ");
+            string?[] header = new string[11];
+            for (int i = 0; i < 11; i++)
+            {
+                //Processing row
+                header[i] = parser.ReadLine();
+            }
+            for (int i = 0; i < 50; i++)
+            {
+                var fields = parser.ReadFields();
+                if(fields != null)
+                {
+                    foreach (string field in fields)
+                    {
+                        Console.WriteLine(i);
+                        Console.WriteLine(field);
+                    }
+                }
+                
+            }
+                
+        }
+    }
+}
